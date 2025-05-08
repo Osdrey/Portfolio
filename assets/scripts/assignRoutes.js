850dac6fd0d6406b5cf755faa712a10788210e53
Osdrey <oscardaniloreyesb@gmail.com>
2025-05-07 22:57:48 -0500

    Agregando rutas
    Se agregan todas las rutas que dirigirán a las demás secciones, se crea un script, en caso de que cambie la ruta, solo deberá cambiarse allí y no en cada elemento del html.

---
+++ assets/scripts/assignRoutes.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('homeRouteHeader').href = routes.homeRouteHeader;
+    document.getElementById('homeRoute').href = routes.homeRoute;
+    document.getElementById('aboutMeRoute').href = routes.aboutMeRoute;
+    document.getElementById('projectsRoute').href = routes.projectsRoute;
+    document.getElementById('skillsRoute').href = routes.skillsRoute;
+    document.getElementById('contactRoute').href = routes.contactRoute;
+});
